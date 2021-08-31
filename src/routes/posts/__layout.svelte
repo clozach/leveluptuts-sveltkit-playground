773e90b491cf28cd9284a3a2fb5af6361167dfb1
Elbong Gearny <elbong@navelgazer.club>
2021-08-31 16:54:07 -0700

    Nested Layouts

---
+++ src/routes/posts/__layout.svelte
@@ -0,0 +1,23 @@
+<div>
+	<slot>§ I Am Default §</slot>
+
+	<aside>
+		<h5>Archive</h5>
+		<ul>
+			<li><a href="/posts">Home (blog)</a></li>
+			<li><a href="/posts/about">About (blog)</a></li>
+			<li><a href="/">Home (site)</a></li>
+			<li><a href="/about">About (site)</a></li>
+		</ul>
+	</aside>
+</div>
+
+<style lang="postcss">
+	div {
+		background-color: wheat;
+	}
+	a {
+		color: green;
+		@apply underline;
+	}
+</style>
