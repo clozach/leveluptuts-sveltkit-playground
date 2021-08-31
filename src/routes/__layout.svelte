9ebbe090da6ca719934f62626bd928f4b583663f
Elbong Gearny <elbong@navelgazer.club>
2021-08-31 15:18:22 -0700

    Pages, Routing and Layouts In Svelte Kit: slotting page content into a `__layout.svelte`

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,20 @@
+<header>
+	<h1>Nav</h1>
+</header>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About Foo</a>
+</nav>
+
+<main>
+	<slot />
+</main>
+
+<footer>Copywrite © 2021 Elbong Q. Gearny</footer>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
