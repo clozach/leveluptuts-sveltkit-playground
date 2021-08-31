d5e27c3ebf22b0fc7b2f0594c5b5ec51e83f6405
Elbong Gearny <elbong@navelgazer.club>
2021-08-31 15:50:00 -0700

    `npm install --save node-sass` + config & `lang="scss"`, and we have preprocessing!

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,14 +1,22 @@
 <header>
 	<h1>Nav</h1>
+
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About Foo</a>
+	</nav>
 </header>
 
-<nav>
-	<a href="/">Home</a>
-	<a href="/about">About Foo</a>
-</nav>
-
-<style>
-	h1 {
+<style lang="scss">
+	header {
 		color: red;
+		nav {
+			:visited {
+				color: white;
+			}
+			color: white;
+			background: blue;
+			padding: 1rem;
+		}
 	}
 </style>
