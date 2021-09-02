9609e82779fc9829b8a5b8f3497d964e9b4bde44
Elbong Gearny <elbong@navelgazer.club>
2021-09-02 13:13:56 -0700

    Loading Data Before Initial Render: this file demonstrates use of the `load` function to provide context to slug-based pages
    Also through in an auto-formatting whitespace change resulting from saving .prettierrc after turning of VS Code's editor.detectIndentation setting, which was causing everything to save with leading tabs instead of spaces

---
+++ src/routes/posts/[slug].svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	export async function load({ page, fetch, session, context }) {
+		const post = {
+			title: page.params.slug,
+			date: new Date(),
+			body: `Lorem ipsum dolor sit amet,…`
+		};
+		return {
+			props: { post }
+		};
+	}
+</script>
+
+<script>
+	export let post;
+</script>
+
+<h3>{post.title}</h3>
+<p>{post.body}</p>
