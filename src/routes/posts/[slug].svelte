7657b3e64c2a0c70ed9ad3c7e925270d6bf3cf8d
Elbong Gearny <elbong@navelgazer.club>
2021-09-02 15:06:59 -0700

    Minor - cleanup

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,25 +1,14 @@
 <script context="module">
   export async function load({ page, fetch, session, context }) {
     const Post = await import(`../../posts/${page.params.slug}.md`);
-    console.log(Post, Post.metadata);
-    const post = {
-      title: page.params.slug,
-      date: new Date(),
-      body: `Lorem ipsum dolor sit amet,…`
-    };
     return {
-      props: { Post: Post.default, post, title: `🧧${Post.metadata?.title}` }
+      props: { Post: Post.default }
     };
   }
 </script>
 
 <script>
-  export let post;
-  export let title;
   export let Post;
 </script>
 
 <Post />
-<h2>{title}</h2>
-<h3>{post.title}</h3>
-<p>{post.body}</p>
