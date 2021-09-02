8238ec1547d66a98892b3e1a484171294905cd85
Elbong Gearny <elbong@navelgazer.club>
2021-09-02 13:14:11 -0700

    `npx svelte-add mdsvex`
    + `npm i`

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
+import { mdsvex } from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js";
 import sveltePreprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
+    "extensions": [".svelte", ...mdsvexConfig.extensions],
+
+    kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
-	preprocess: sveltePreprocess()
+
+    preprocess: [sveltePreprocess(), mdsvex(mdsvexConfig)]
 };
 
 export default config;
