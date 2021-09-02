af9ccf8f1ad7a493d03955b116b428ecf0b482ec
Elbong Gearny <elbong@navelgazer.club>
2021-09-02 13:27:58 -0700

    Trivial Markdown example

---
+++ src/posts/hello.md
@@ -0,0 +1,15 @@
+# Hello Whirled
+
+- first
+- second
+- third
+
+1. Uno
+2. Dos
+3. Tres
+
+<style>
+  h1 {
+    color: blue;
+  }
+</style>
--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,19 +1,21 @@
 <script context="module">
-	export async function load({ page, fetch, session, context }) {
-		const post = {
-			title: page.params.slug,
-			date: new Date(),
-			body: `Lorem ipsum dolor sit amet,…`
-		};
-		return {
-			props: { post }
-		};
-	}
+  export async function load({ page, fetch, session, context }) {
+    const post = {
+      title: page.params.slug,
+      date: new Date(),
+      body: `Lorem ipsum dolor sit amet,…`
+    };
+    return {
+      props: { post }
+    };
+  }
 </script>
 
 <script>
-	export let post;
+  import Hello from '../../posts/hello.md';
+  export let post;
 </script>
 
+<Hello />
 <h3>{post.title}</h3>
 <p>{post.body}</p>
