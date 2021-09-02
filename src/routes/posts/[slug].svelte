a88ef32565b1a21754bab72ad4110310dfc5498e
Elbong Gearny <elbong@navelgazer.club>
2021-09-02 14:09:01 -0700

    Dynamic Slug + Markdown

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,22 +1,22 @@
 <script context="module">
   export async function load({ page, fetch, session, context }) {
-    const Hello = (await import('../../posts/hello.md')).default;
+    const Post = (await import(`../../posts/${page.params.slug}.md`)).default;
     const post = {
       title: page.params.slug,
       date: new Date(),
       body: `Lorem ipsum dolor sit amet,…`
     };
     return {
-      props: { Hello, post }
+      props: { Post, post }
     };
   }
 </script>
 
 <script>
   export let post;
-  export let Hello;
+  export let Post;
 </script>
 
-<Hello />
+<Post />
 <h3>{post.title}</h3>
 <p>{post.body}</p>
