ddd043cfb1a60cd00c000973deb71568624dfb16
Elbong Gearny <elbong@navelgazer.club>
2021-09-02 20:09:35 -0700

    Wired-in static-site adapter
    npm run build + npm run preview to test

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import sveltePreprocess from "svelte-preprocess"
+import adapter from "@sveltejs/adapter-static"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   "extensions": [".svelte", ...mdsvexConfig.extensions],
 
   kit: {
+    adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
   },
